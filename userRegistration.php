d9bd867410454f69403b1c000e0c762e79c9ee9a
Jacktwison <jacktwison@gmail.com.com>
2024-03-18 16:03:51 +0200

    Data add
    data add successfull

---
+++ userRegistration.php
@@ -0,0 +1,15 @@
+<html>
+    <head><title>I CODE LTd</title>
+</head>
+<body bgcolor="Gold">
+    <form action="" method="POST">
+        <table border="0">
+            <tr><td colspan="2"><center><h1><b>User Registration Form</b></center></td></tr>
+            <tr><td><label>Names</label></td><td><input type="text" name="name" placeholder="Enter your Names"></td></tr>
+            <tr><td><label>Names</label></td><td><input type="email" name="email" placeholder="Enter your email"></td></tr>
+            <tr><td><label>Names</label></td><td><input type="password" name="password" placeholder="Enter your password"></td></tr>
+            <tr><td colspan="2"><center><h1><b><input type="submit" name="btn" value="Submit"></b></center></td></tr>
+</table>
+</Form>
+</body>
+</html>
